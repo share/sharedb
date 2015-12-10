876993fdc07f26075397cb41fd5c28776175c85f
Nate Smith <nate@nateps.com>
2015-12-09 23:35:08 -0800

    add test for bug with subscribing or fetching the same document twice in the same bulk action

--- test/client/subscribe.js
+++ test/client/subscribe.js
@@ -4,6 +4,80 @@ var async = require('async');
 
 describe('client subscribe', function() {
 
+  ['fetch', 'subscribe'].forEach(function(method) {
+    it(method + ' gets initial data', function(done) {
+      var backend = new Backend();
+      var doc = backend.connect().get('dogs', 'fido');
+      var doc2 = backend.connect().get('dogs', 'fido');
+      doc.create('json0', {age: 3}, function(err) {
+        if (err) return done(err);
+        doc2[method](function(err) {
+          if (err) return done(err);
+          expect(doc2.version).eql(1);
+          expect(doc2.snapshot).eql({age: 3})
+          done();
+        });
+      });
+    });
+
+    it(method + ' twice simultaneously calls back', function(done) {
+      var backend = new Backend();
+      var doc = backend.connect().get('dogs', 'fido');
+      var doc2 = backend.connect().get('dogs', 'fido');
+      doc.create('json0', {age: 3}, function(err) {
+        if (err) return done(err);
+        async.parallel([
+          function(cb) { doc2.fetch(cb); },
+          function(cb) { doc2.fetch(cb); }
+        ], function(err) {
+          if (err) return done(err);
+          expect(doc2.version).eql(1);
+          expect(doc2.snapshot).eql({age: 3})
+          done();
+        });
+      });
+    });
+
+    it(method + ' twice in bulk simultaneously calls back', function(done) {
+      var backend = new Backend();
+      var doc = backend.connect().get('dogs', 'fido');
+      var doc2 = backend.connect().get('dogs', 'fido');
+      doc.create('json0', {age: 3}, function(err) {
+        if (err) return done(err);
+        doc2.connection.startBulk();
+        async.parallel([
+          function(cb) { doc2.fetch(cb); },
+          function(cb) { doc2.fetch(cb); }
+        ], function(err) {
+          if (err) return done(err);
+          expect(doc2.version).eql(1);
+          expect(doc2.snapshot).eql({age: 3})
+          done();
+        });
+        doc2.connection.endBulk();
+      });
+    });
+
+    it(method + ' gets new ops', function(done) {
+      var backend = new Backend();
+      var doc = backend.connect().get('dogs', 'fido');
+      var doc2 = backend.connect().get('dogs', 'fido');
+      doc.create('json0', {age: 3}, function(err) {
+        if (err) return done(err);
+        doc2.fetch(function(err) {
+          if (err) return done(err);
+          doc.submitOp({p: ['age'], na: 1}, function(err) {
+            if (err) return done(err);
+            doc2.on('op', function(op, context) {
+              done();
+            });
+            doc2[method]();
+          });
+        });
+      });
+    });
+  });
+
   it('subscribed client gets create from first client', function(done) {
     var backend = new Backend();
     var doc = backend.connect().get('dogs', 'fido');
@@ -17,59 +91,6 @@ describe('client subscribe', function() {
         done();
       });
       doc.create('json0', {age: 3});
-    });
-  });
-
-  it('subscribe fetches initial data', function(done) {
-    var backend = new Backend();
-    var doc = backend.connect().get('dogs', 'fido');
-    var doc2 = backend.connect().get('dogs', 'fido');
-    doc.create('json0', {age: 3}, function(err) {
-      if (err) return done(err);
-      doc2.subscribe(function(err) {
-        if (err) return done(err);
-        expect(doc2.version).eql(1);
-        expect(doc2.snapshot).eql({age: 3})
-        done();
-      });
-    });
-  });
-
-  it('fetch fetches new ops', function(done) {
-    var backend = new Backend();
-    var doc = backend.connect().get('dogs', 'fido');
-    var doc2 = backend.connect().get('dogs', 'fido');
-    doc.create('json0', {age: 3}, function(err) {
-      if (err) return done(err);
-      doc2.fetch(function(err) {
-        if (err) return done(err);
-        doc.submitOp({p: ['age'], na: 1}, function(err) {
-          if (err) return done(err);
-          doc2.on('op', function(op, context) {
-            done();
-          });
-          doc2.fetch();
-        });
-      });
-    });
-  });
-
-  it('subscribe fetches new ops', function(done) {
-    var backend = new Backend();
-    var doc = backend.connect().get('dogs', 'fido');
-    var doc2 = backend.connect().get('dogs', 'fido');
-    doc.create('json0', {age: 3}, function(err) {
-      if (err) return done(err);
-      doc2.fetch(function(err) {
-        if (err) return done(err);
-        doc.submitOp({p: ['age'], na: 1}, function(err) {
-          if (err) return done(err);
-          doc2.on('op', function(op, context) {
-            done();
-          });
-          doc2.subscribe();
-        });
-      });
     });
   });
 
