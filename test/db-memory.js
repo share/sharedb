7eba7d0e884f246ba4278dd24202754e46c55fdf
Nate Smith <nate@nateps.com>
2015-12-27 01:23:25 -0800

    test db class unimplemented errors

--- test/db-memory.js
+++ test/db-memory.js
@@ -1,4 +1,53 @@
+var expect = require('expect.js');
+var DB = require('../lib/db');
 var MemoryDB = require('../lib/db/memory');
+
+describe('DB base class', function() {
+  it('can call db.close()', function() {
+    var db = new DB();
+    db.close();
+  });
+
+  it('returns an error if db.commit() is unimplemented', function(done) {
+    var db = new DB();
+    db.commit('testcollection', 'test', {}, {}, function(err) {
+      expect(err).an(Error);
+      done();
+    });
+  });
+
+  it('returns an error if db.getSnapshot() is unimplemented', function(done) {
+    var db = new DB();
+    db.getSnapshot('testcollection', 'foo', null, function(err) {
+      expect(err).an(Error);
+      done();
+    });
+  });
+
+  it('returns an error if db.getOps() is unimplemented', function(done) {
+    var db = new DB();
+    db.getOps('testcollection', 'foo', 0, null, function(err) {
+      expect(err).an(Error);
+      done();
+    });
+  });
+
+  it('returns an error if db.query() is unimplemented', function(done) {
+    var db = new DB();
+    db.query('testcollection', {x: 5}, null, null, function(err) {
+      expect(err).an(Error);
+      done();
+    });
+  });
+
+  it('returns an error if db.queryPollDoc() is unimplemented', function(done) {
+    var db = new DB();
+    db.queryPollDoc('testcollection', 'foo', {x: 5}, null, function(err) {
+      expect(err).an(Error);
+      done();
+    });
+  });
+});
 
 require('./db')(function(callback) {
   var db = new MemoryDB();
