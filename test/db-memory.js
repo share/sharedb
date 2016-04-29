10008c6cd516f6afcf9ed85a09814cdf2d369196
Avital Oliver <avital@theWE.net>
2016-04-29 02:01:25 -0700

    Use sharedb-mingo-memory instead of custom MemoryDB in tests

--- test/db-memory.js
+++ test/db-memory.js
@@ -1,6 +1,6 @@
 var expect = require('expect.js');
 var DB = require('../lib/db');
-var MemoryDB = require('../lib/db/memory');
+var MingoMemoryDB = require('sharedb-mingo-memory');
 
 describe('DB base class', function() {
   it('can call db.close() without callback', function() {
@@ -55,60 +55,7 @@ describe('DB base class', function() {
 });
 
 require('./db')(function(callback) {
-  var db = new MemoryDB();
-
-  // Implement extremely simple subset of Mongo queries for unit tests
-  db._querySync = function(snapshots, query) {
-    var filtered = filter(snapshots, query.$query || query);
-    sort(filtered, query.$orderby);
-    return filtered;
-  };
-
-  db.queryPollDoc = function(collection, id, query, options, callback) {
-    this.getSnapshot(collection, id, null, function(err, snapshot) {
-      if (err) return callback(err);
-      var result = filterSnapshot(snapshot, query);
-      callback(null, result);
-    });
-  };
-
-  db.canPollDoc = function(collection, query) {
-    return !query.$orderby;
-  };
-
+  var db = new MingoMemoryDB();
   callback(null, db);
 });
 
-// Support exact key match filters only
-function filter(snapshots, query) {
-  return snapshots.filter(function(snapshot) {
-    return filterSnapshot(snapshot, query);
-  });
-}
-
-function filterSnapshot(snapshot, query) {
-  if (!snapshot.data) return false;
-  for (var key in query) {
-    if (key.charAt(0) === '$') continue;
-    if (snapshot.data[key] !== query[key]) return false;
-  }
-  return true;
-}
-
-// Support sorting with the Mongo $orderby syntax
-function sort(snapshots, orderby) {
-  if (!orderby) return;
-  snapshots.sort(function(snapshotA, snapshotB) {
-    for (var key in orderby) {
-      var value = orderby[key];
-      if (value !== 1 && value !== -1) {
-        throw new Error('Invalid $orderby value');
-      }
-      var a = snapshotA.data && snapshotA.data[key];
-      var b = snapshotB.data && snapshotB.data[key];
-      if (a > b) return value;
-      if (b > a) return -value;
-    }
-    return 0;
-  });
-}
