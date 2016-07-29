b9e3b2be64145555a34242f558d33112215b4537
Nate Smith <nate@nateps.com>
2016-07-28 18:17:21 -0700

    remove hardcoded host in leaderboard example

--- examples/leaderboard/client/index.jsx
+++ examples/leaderboard/client/index.jsx
@@ -5,7 +5,7 @@ var ReactDOM = require('react-dom');
 var sharedb = require('sharedb/lib/client');
 
 // Open WebSocket connection to ShareDB server
-connection = new sharedb.Connection(new WebSocket('ws://localhost:8080'));
+connection = new sharedb.Connection(new WebSocket('ws://' + window.location.host));
 
 // Expose to index.html
 window.renderBody = function() {
