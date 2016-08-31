358d198421135446c16b11919dde3d381b9b02b4
curran <curran.kelleher@gmail.com>
2016-08-31 13:13:29 +0530

    Refactor connection from global into module. Closes #118

--- examples/leaderboard/client/index.jsx
+++ examples/leaderboard/client/index.jsx
@@ -2,11 +2,6 @@ var Body = require('./Body.jsx');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var sharedb = require('sharedb/lib/client');
-
-// Open WebSocket connection to ShareDB server
-connection = new sharedb.Connection(new WebSocket('ws://' + window.location.host));
-
 // Expose to index.html
 window.renderBody = function() {
   ReactDOM.render(<Body />, document.querySelector('#main'));
