1e8f3e932f960c171c47e16a2966f1873e280c7d
curran <curran.kelleher@gmail.com>
2016-09-27 16:49:42 +0530

    Simplify and document server code

--- examples/textarea-routing/server.js
+++ examples/textarea-routing/server.js
@@ -1,4 +1,6 @@
+// This is our ShareDB server, built on Express and WebSockets.
 // Derived from https://github.com/share/sharedb/blob/master/examples/textarea/server.js
+
 var http = require('http');
 var express = require('express');
 var ShareDB = require('sharedb');
@@ -6,30 +8,27 @@ var WebSocket = require('ws');
 var WebSocketJSONStream = require('websocket-json-stream');
 var path = require('path');
 
+var buildDir = 'build';
+
 var backend = new ShareDB();
+var app = express();
 
-function startServer() {
+// Serve static assets bundled by `npm run build`.
+app.use(express.static(buildDir));
 
-  // Create a web server to serve files and listen to WebSocket connections
-  var app = express();
+// Serve index.html for any route that doesn't match static assets,
+// for compatibility with react-router browserHistory.
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, buildDir, 'index.html'))
+})
 
-  app.use(express.static('build'));
+var server = http.createServer(app);
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  })
-
-  var server = http.createServer(app);
-
-  // Connect any incoming WebSocket connection to ShareDB
-  var wss = new WebSocket.Server({server: server});
-  wss.on('connection', function(ws, req) {
+new WebSocket.Server({server: server})
+  .on('connection', function(ws, req) {
     var stream = new WebSocketJSONStream(ws);
     backend.listen(stream);
   });
 
-  server.listen(8080);
-  console.log('Listening on http://localhost:8080');
-}
-
-startServer();
+server.listen(8080);
+console.log('Listening on http://localhost:8080');
