b29334057c26156b9037816f8f5766cb8a8563fd
curran <curran.kelleher@gmail.com>
2016-10-02 00:55:41 +0530

    Refactor where doc is obtained

--- examples/textarea-routing/src/ShareDBStringBinding.js
+++ examples/textarea-routing/src/ShareDBStringBinding.js
@@ -6,45 +6,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import StringBinding from 'sharedb-string-binding';
-import connection from './connection';
 import './TextPad.css';
 
-function createIfNeeded(doc, callback){
-  if(doc.type === null){
-    doc.create('', callback);
-  } else {
-    callback();
-  }
-}
 
 export default class ShareDBStringBinding extends Component {
 
   componentDidMount() {
-
-    const { collection, id } = this.props;
-
-    const doc = connection.get(collection, id);
+    const { doc } = this.props;
     const textArea = ReactDOM.findDOMNode(this.refs.stringDOM);
-
     doc.subscribe((err) => {
       if (err) throw err;
-      createIfNeeded(doc, () => {
-        const binding = new StringBinding(textArea, doc);
-        binding.setup();
-      });
+      const binding = new StringBinding(textArea, doc);
+      binding.setup();
     });
-
-    this.doc = doc;
-  }
-
-  componentWillUnmount() {
-    this.doc.destroy();
   }
 
   render() {
+    const { type, className } = this.props;
     return React.createElement(
-      this.props.type,
-      Object.assign({ ref: 'stringDOM' }, this.props)
+      type,
+      {
+        ref: 'stringDOM',
+        type,
+        className
+      }
     );
   }
 }
--- examples/textarea-routing/src/TextPad.js
+++ examples/textarea-routing/src/TextPad.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import ShareDBStringBinding from './ShareDBStringBinding';
+import connection from './connection';
 import './TextPad.css';
 
-export default ({id}) => {
-  return (
-    <ShareDBStringBinding
-      type='textarea'
-      collection='textPads'
-      id={id} 
-      className='text-pad'
-    />
-  );
+function createIfNeeded(doc, callback){
+  if(doc.type === null){
+    doc.create('', callback);
+  } else {
+    callback();
+  }
+}
+
+export default class TextPad extends Component {
+  componentWillMount() {
+    const collection = 'textPads';
+    const doc = connection.get(collection, this.props.id);
+    doc.subscribe((err) => {
+      createIfNeeded(doc, () => {
+      });
+    });
+    this.doc = doc;
+  }
+  render (){
+    return (
+      <ShareDBStringBinding
+        type='textarea'
+        doc={this.doc}
+        className='text-pad'
+      />
+    );
+  }
 };
