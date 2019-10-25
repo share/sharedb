7b06e962f673eb2a4dcef1eab5a137004917f485
Nate Smith <nate@lever.co>
2019-10-25 15:44:04 -0700

    add test coverage for queryPollDoc
    implmenet additional BasicQueryableMemoryDB implementation methods, so that code paths in query-emitter are exercised by tests

--- test/BasicQueryableMemoryDB.js
+++ test/BasicQueryableMemoryDB.js
@@ -14,17 +14,7 @@ BasicQueryableMemoryDB.prototype.constructor = BasicQueryableMemoryDB;
 BasicQueryableMemoryDB.prototype._querySync = function(snapshots, query) {
   if (query.filter) {
     snapshots = snapshots.filter(function(snapshot) {
-      for (var queryKey in query.filter) {
-        // This fake only supports simple property equality filters, so
-        // throw an error on Mongo-like filter properties with dots.
-        if (queryKey.includes('.')) {
-          throw new Error('Only simple property filters are supported, got:', queryKey);
-        }
-        if (snapshot.data[queryKey] !== query.filter[queryKey]) {
-          return false;
-        }
-      }
-      return true;
+      return querySnapshot(snapshot, query);
     });
   }
 
@@ -39,6 +29,42 @@ BasicQueryableMemoryDB.prototype._querySync = function(snapshots, query) {
 
   return {snapshots: snapshots};
 };
+
+BasicQueryableMemoryDB.prototype.queryPollDoc = function(collection, id, query, options, callback) {
+  var db = this;
+  process.nextTick(function() {
+    var snapshot = db._getSnapshotSync(collection, id);
+    try {
+      var matches = querySnapshot(snapshot, query);
+    } catch (err) {
+      return callback(err);
+    }
+    callback(null, matches);
+  });
+};
+
+BasicQueryableMemoryDB.prototype.canPollDoc = function(collection, query) {
+  return !query.sort;
+};
+
+function querySnapshot(snapshot, query) {
+  // Never match uncreated or deleted snapshots
+  if (snapshot.type == null) return false;
+  // Match any snapshot when there is no query filter
+  if (!query.filter) return true;
+  // Check that each property in the filter equals the snapshot data
+  for (var queryKey in query.filter) {
+    // This fake only supports simple property equality filters, so
+    // throw an error on Mongo-like filter properties with dots.
+    if (queryKey.includes('.')) {
+      throw new Error('Only simple property filters are supported, got:', queryKey);
+    }
+    if (snapshot.data[queryKey] !== query.filter[queryKey]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // sortProperties is an array whose items are each [propertyName, direction].
 function snapshotComparator(sortProperties) {
