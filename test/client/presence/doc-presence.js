5831da64af50d13d78d24bc2b69caeca511a5be5
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2023-10-12 15:40:20 +0100

    ♻️ Remove `presence.subscribe()` `force` option
    We previously only needed this to re-request presence as part of the
    `DocPresence` catchup flow.
    
    This change adds an internal method to actively request remote presence
    for a given channel, and uses that instead of abusing `subscribe()`.
    This lets us remove the `options` object from `subscribe()`.

--- test/client/presence/doc-presence.js
+++ test/client/presence/doc-presence.js
@@ -645,20 +645,14 @@ describe('DocPresence', function() {
       presence1.subscribe.bind(presence1),
       presence2.subscribe.bind(presence2),
       function(next) {
-        connection1._setState('disconnected');
-        next();
-      },
-      function(next) {
-        localPresence2.submit({index: 0}, errorHandler(done));
-        // We've not _actually_ disconnected the connection, so this
-        // event will still fire.
-        presence1.once('receive', function() {
+        connection1.once('closed', function() {
           next();
         });
+        connection1.close();
       },
+      localPresence2.submit.bind(localPresence2, {index: 0}),
       function(next) {
-        connection1._setState('connecting');
-        connection1._setState('connected');
+        backend.connect(connection1);
         presence1.once('receive', function(id, presence) {
           expect(presence).to.eql({index: 0});
           next();
