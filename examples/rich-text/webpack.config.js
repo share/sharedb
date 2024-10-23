2f39ee7c29d409644ad1db4bef8944eafabd5150
Doug Horner <horner@mieweb.com>
2024-10-23 00:12:36 -0400

    Removed babel dependancy

--- examples/rich-text/webpack.config.js
+++ examples/rich-text/webpack.config.js
@@ -11,12 +11,6 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
       }
     ]
   },
